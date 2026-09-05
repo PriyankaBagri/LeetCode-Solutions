3d5e468a2132e89c916a0a033ffcd5c15639a072
PriyankaBagri <bagripriyanka572@gmail.com>
2026-09-05 17:04:00 +0530

    Time: 18 ms (26.96%), Space: 210.8 MB (35.81%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+      int n = nums.size();
+       vector<int> prefix_max(n);
+        prefix_max[0] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            prefix_max[i] = std::max(prefix_max[i - 1], nums[i]);
+        }
+        
+        vector<int> suffix_min(n);
+        suffix_min[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suffix_min[i] = std::min(suffix_min[i + 1], nums[i]);
+        }
+        
+       
+        for (int i = 0; i < n; ++i) {
+            if (prefix_max[i] - suffix_min[i] <= k) {
+                return i;
+            }
+        }
+        
+        return -1;  
+    }
+};
